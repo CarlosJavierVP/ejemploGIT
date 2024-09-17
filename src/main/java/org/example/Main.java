925bd6fa787073f7023abd6e6f8a031511b14428
CarlosJavierValenzue <carlos.valenzuela.j92@gmail.com>
2024-09-17 08:27:31 +0200

    Commit ejercicio terminado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,48 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        public static boolean esPrimo(Integer num){
+            boolean primo = true;
+            if (num <= 1){
+                primo = false;
+            }
 
-        for (int i = 1; i <= 5; i++) {
+            for (int i = 2; i <= Math.sqrt(num); i++){
+                if (num % i == 0){
+                    primo = false;
+                }
+            }
+            return primo;
+        }
 
-            System.out.println("i = " + i);
+        public static ArrayList<Integer> listPrimos(int first, int last){
+            ArrayList <Integer> list= new ArrayList<>();
+            for (int i = first; i<=last; i++){
+                if (esPrimo(i)){
+                    list.add(i);
+                }
+            }
+            System.out.println(list);
+            return list;
+        }
+
+
+        public static void main(String[] args) {
+            System.out.println("BUSCADOR DE PRIMOS"+'\n'+"==================");
+
+            int first = 1;
+            int last = 1;
+            Scanner sc = new Scanner(System.in);
+            do {
+                System.out.println("Escriba el número inicial (0 para salir):");
+                first = sc.nextInt();
+                System.out.println("Escriba el número final:");
+                last = sc.nextInt();
+
+                listPrimos(first, last);
+
+            }while (first != 0);
+            sc.close();
         }
     }
 }
